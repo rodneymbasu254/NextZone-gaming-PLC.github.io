8b78d6fdc6e4246cb8b9eaea53318ec9f76786ae
Rodney Mbasu <rodneymbasu@gmail.com>
2023-07-06 15:34:04 +0300

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,187 @@
+var buttonContainer = document.getElementById("buttonContainer");
+var startButton = document.getElementById('start');
+
+var animationRunning = false;
+
+var onPlay = new Audio("Audio/mixkit-cinematic-horror-heartbeat-transition-489.wav");
+
+function startGame() {
+  onPlay.play();
+  if (!animationRunning) {
+    animationRunning = true;
+    var block = document.querySelector("#block");
+    block.style.display = "block";
+    var object = document.querySelector("#object img");
+    object.style.display = "block"
+    startButton.textContent = "Pause";
+  } else {
+    onPlay.pause();
+    gamePause();
+  }
+};
+
+function gamePause() {
+  animationRunning = false;
+  var block = document.querySelector("#block");
+  block.style.display = "none";
+  var object = document.querySelector("#object img");
+  object.style.display = "none";
+  startButton.textContent = "Resume";
+}
+buttonContainer.addEventListener("click", function(event) {
+  if (event.target === startButton) {
+    startGame();
+  }
+});
+
+var animationImages = [
+  "img/character_zombie_attack1.png",
+  "img/character_zombie_attack2.png",
+  "img/character_zombie_attackKick.png",
+];
+var character = document.getElementById("character");
+var currentImageIndex = 0;
+
+function jump() {
+  character.style.backgroundImage = "url(" + animationImages[1] + ")";
+}
+
+function removeJump() {
+  character.style.backgroundImage = "url(" + animationImages[2] + ")";
+}
+
+var onJump = new Audio("footstep_grass_004.mp3");
+function jump() {
+  if (character.classList == "animate") {
+    return;
+  }
+  onJump.play();
+  character.classList.add("animate");
+  setTimeout(removeJump, 300);
+};
+
+function removeJump() {
+  character.classList.remove("animate");
+}
+var startJump = document.getElementById("jump");
+buttonContainer.addEventListener("click", function(event) {
+  if (event.target === startJump) {
+    jump();
+  }
+});
+
+var block = document.getElementById("block");
+
+var onCollide = new Audio("impactWood_heavy_003.ogg");
+function checkDead() {
+  let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+  if (blockLeft < 20 && blockLeft >-20 && characterTop >= 130) {
+    onCollide.play();
+    gameOver();
+  }
+  if (animationRunning) {
+    reward(1);
+  }
+};
+
+setInterval(checkDead, 10);
+
+var images = ["img/bush1.png", "img/treeLong.png", "img/tree.png", "img/cactus1.png"];
+
+function getRandomImage() {
+  var randomImage = images[Math.floor(Math.random() * images.length)];
+  var imageElement = document.createElement("img");
+  imageElement.src = randomImage;
+  imageElement.addEventListener("animationend", function() {
+    this.parentNode.removeChild(this);
+    getRandomImage();
+  });
+
+  var object = document.getElementById("object");
+  object.appendChild(imageElement);
+
+}
+
+getRandomImage();
+
+var score = 0;
+
+function reward(points) {
+  score += points;
+  var scoreElement = document.getElementById("score");
+  scoreElement.textContent = "score: " + score;
+};
+
+function resetScore() {
+  score = 0;
+  var scoreElement = document.getElementById("score");
+  scoreElement.textContent = "Score: " + score;
+}
+
+function retry() {
+  onPlay.play();
+  var retVal = confirm ("exit?");
+  if (retVal == true) {
+    Redirect();
+    return true;
+  } else {
+    resetScore();
+    startGame();
+    startButton.textContent = "Retry";
+    return false;
+  }
+}
+
+function Redirect() {
+  window.location = "gameOver.html";
+}
+
+function gameOver() {
+  onPlay.pause();
+  alert(`GameOver your score is ${score}`);
+  retry();
+};
+
+var currentScoreElement = document.getElementById("currentScore");
+var saveScoreButton = document.getElementById("saveScoreButton");
+var highScoresList = document.getElementById("highScoresList");
+
+function saveScore(score) {
+  var scores = getScoresFromStorage();
+
+  scores.push(score);
+  scores.sort((a, b) => b - a);
+
+  localStorage.setItem("scores", JSON.stringify(scores));
+}
+
+function getScoresFromStorage() {
+  var scores = localStorage.getItem("scores");
+
+  if (scores) {
+    return JSON.parse(scores);
+  } else {
+    return [];
+  }
+}
+
+function displayHighScores() {
+  var scores = getScoresFromStorage();
+
+  highScoresList.innerHTML = "";
+
+  for (var i = 0; i < scores.length; i++) {
+    var scoreItem = document.createElement("li");
+    scoreItem.textContent = scores[i];
+    highScoresList.appendChild(scoreItem);
+  }
+}
+
+saveScoreButton.addEventListener("click", function() {
+  var currentScore = parseInt(currentScoreElement.textContent);
+  saveScore(currentScore);
+  displayHighScores();
+});
+
+displayHighScores();
---
+++ js/index.js
@@ -0,0 +1,83 @@
+var images = [
+  "img/backgroundColorGrass.png",
+  "img/backgroundColorForest.png",
+  "img/backgroundColorFall.png",
+  "img/backgroundColorDesert.png",
+  "img/backgroundDesert.png",
+];
+
+var index = 0;
+var slideshowElement = document.getElementById("backgroundSlideshow");
+
+function changeBackgroundImage() {
+  slideshowElement.style.backgroundImage = "url(" + images[index] + ")";
+  index = (index + 1) % images.length;
+}
+
+setInterval(changeBackgroundImage, 3000);
+
+var words = [
+  "You gotta be kidding, why are they after me??",
+  "I hate this",
+  "Wait!! Why am I running?",
+  "Being chased by humans is like escaping wind",
+  "Believe me I'm a good monster"
+];
+
+var captionElement = document.getElementById("caption");
+function changeCaption(index) {
+  captionElement.textContent = words[index];
+  captionElement.style.animation = "fadeInOut 2s ease-in-out";
+  captionElement.addEventListener("animationend", function() {
+    captionElement.style.animation = "none";
+    setTimeout(function() {
+      if (index < words.length - 1) {
+        changeCaption(index + 1);
+      } else {
+        restart();
+      }
+    }, 5000);
+  });
+}
+changeCaption(0);
+
+function restart() {
+  index = 0;
+  changeCaption(index);
+}
+
+var startButton = document.getElementById("begin");
+var startSound = new Audio("Audio/click5.ogg");
+function redirect() {
+  window.location = "game.html";
+}
+var progressBar = document.getElementById("progressBar");
+
+var animation = false;
+
+function simulateProgress() {
+  if (!animation) {
+    animation = true;
+    var progressBar = document.querySelector("#progressBar");
+    progressBar.style.display = "block";
+    var width = 0;
+    var interval = setInterval(frame, 10);
+
+    function frame() {
+      if (width >= 100) {
+        clearInterval(interval);
+        redirect();
+      } else {
+        width++;
+        progressBar.style.width = width + "%";
+      }
+    }
+  }
+}
+
+startButton.addEventListener("click", function(event) {
+  startSound.play();
+  if (event.target === startButton) {
+    simulateProgress();
+  }
+});
